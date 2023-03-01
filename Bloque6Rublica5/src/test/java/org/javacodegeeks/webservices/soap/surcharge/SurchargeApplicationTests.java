fd3bf7e58764a52a706b7770f65001fd1afebdfd
SoyAlba <albamanas2003@gmail.com>
2023-03-01 19:13:20 +0100

    Bloque6Rublica5

---
+++ Bloque6Rublica5/src/test/java/org/javacodegeeks/webservices/soap/surcharge/SurchargeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.javacodegeeks.webservices.soap.surcharge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SurchargeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
